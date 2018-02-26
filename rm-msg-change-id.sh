b6478ef055cdfc02dc5d73ae39c51dab9e9ea825
David Wang <david.wang2015@outlook.com>
2018-02-26 11:40:57 +0800

    Support patching latest few commnits
    Signed-off-by: David Wang <david.wang2015@outlook.com>

--- rm-msg-change-id.sh
+++ rm-msg-change-id.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Remove the Change-Id from the commit message of current repository
-git filter-branch -f --prune-empty --msg-filter 'sed "/^Change-Id: /"d'
+if [ $# -gt 0 ]; then
+	echo "Patching latest $1 commit(s)"
+	git filter-branch -f --prune-empty --msg-filter 'sed "/^Change-Id: /"d' HEAD~$1..HEAD
+else
+	echo Patching ALL commits
+	git filter-branch -f --prune-empty --msg-filter 'sed "/^Change-Id: /"d'
+fi
